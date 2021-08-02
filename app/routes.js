da6edbb2cf675249e76ea0062f4e4ca83f0b5d5f
David Doherty <david.doherty@engineering.digital.dwp.gov.uk>
2021-08-02 16:29:05 +0100

    addition of accessibility prototypes

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,50 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+router.route('/2-alpha-accessibility/2a-support-options')
+  .post((req, res, next) => {
+    let redirectUrl
+    switch (req.body['support-type']) {
+      case 'online':
+        redirectUrl = '/2-alpha-accessibility/#'
+        break
+      case 'telephone':
+        redirectUrl = '/2-alpha-accessibility/2a-telephone'
+        break
+      case 'textphone':
+        redirectUrl = '/2-alpha-accessibility/2a-textphone'
+        break
+      case 'relay-uk':
+        redirectUrl = '/2-alpha-accessibility/2a-relay-uk'
+        break
+      case 'video-relay':
+      redirectUrl = '/2-alpha-accessibility/2a-video-relay'
+      break
+
+      default:
+        redirectUrl = req.path
+        break
+    }
+    res.redirect(redirectUrl)
+  })
+
+
+router.route('/2-alpha-accessibility/2b-do-you-need-support')
+  .post((req, res, next) => {
+    let redirectUrl
+    switch (req.body['do-you-need-support']) {
+      case 'no':
+        redirectUrl = '/2-alpha-accessibility/#'
+        break
+      case 'yes':
+        redirectUrl = '/2-alpha-accessibility/2b-accessibility-information'
+        break
+
+      default:
+        redirectUrl = req.path
+        break
+    }
+    res.redirect(redirectUrl)
+  })
+
