48a4838f61b87d10b507c1d793e49c6d48fb6659
David Doherty <david.doherty@engineering.digital.dwp.gov.uk>
2021-11-03 13:35:53 +0000

    completed sa&e

--- app/routes.js
+++ app/routes.js
@@ -1667,3 +1667,131 @@ router.route('/alpha-apply-1b/1b-bsl-support')
       })
 
 
+      router.route('/alpha-apply-1c/1c-specialist-equipment-screener')
+        .post((req, res, next) => {
+          let redirectUrl
+          switch (req.body['specialist-equipment-screener']) {
+            case 'Yes':
+              redirectUrl = '/alpha-apply-1c/1c-specialist-equipment-do-you-know-what-specialist-equipment-you-need'
+              break
+            case 'No':
+              redirectUrl = '/alpha-apply-1c/1c-specialist-equipment-check-your-answers-1'
+              break
+
+            default:
+              redirectUrl = req.path
+              break
+          }
+          res.redirect(redirectUrl)
+        })
+
+        router.route('/alpha-apply-1c/1c-specialist-equipment-do-you-know-what-specialist-equipment-you-need')
+          .post((req, res, next) => {
+            let redirectUrl
+            switch (req.body['specialist-equipment-do-you-know-what-specialist-equipment-you-need']) {
+              case 'Yes, I know what I need':
+                redirectUrl = '/alpha-apply-1c/1c-specialist-equipment-workplace-assessment'
+                break
+              case 'No, I do not know what I need':
+                redirectUrl = '/alpha-apply-1c/1c-specialist-equipment-tell-us-ways-that-you-think-it-might-help'
+                break
+
+              default:
+                redirectUrl = req.path
+                break
+            }
+            res.redirect(redirectUrl)
+          })
+
+
+          router.route('/alpha-apply-1c/1c-specialist-equipment-workplace-assessment')
+            .post((req, res, next) => {
+              let redirectUrl
+              switch (req.body['specialist-equipment-workplace-assessment']) {
+                case 'Yes':
+                  redirectUrl = '/alpha-apply-1c/1c-specialist-equipment-workplace-assessment-upload'
+                  break
+                case 'No':
+                  redirectUrl = '/alpha-apply-1c/1c-specialist-equipment-type-of-equipment-1'
+                  break
+
+                default:
+                  redirectUrl = req.path
+                  break
+              }
+              res.redirect(redirectUrl)
+            })
+
+           router.route('/alpha-apply-1c/1c-specialist-equipment-hmrc-table-1')
+             .post((req, res, next) => {
+               let redirectUrl
+               switch (req.body['add-another']) {
+                 case 'Yes':
+                   redirectUrl = '/alpha-apply-1c/1c-specialist-equipment-type-of-equipment-2'
+                   break
+                 case 'No':
+                   redirectUrl = '/alpha-apply-1c/1c-specialist-equipment-check-your-answers-#'
+                   break
+
+                 default:
+                   redirectUrl = req.path
+                   break
+               }
+               res.redirect(redirectUrl)
+             })
+
+             router.route('/alpha-apply-1c/1c-specialist-equipment-hmrc-table-3')
+               .post((req, res, next) => {
+                 let redirectUrl
+                 switch (req.body['add-another-3']) {
+                   case 'Yes':
+                     redirectUrl = '/alpha-apply-1c/#'
+                     break
+                   case 'No':
+                     redirectUrl = '/alpha-apply-1c/1c-specialist-equipment-impact-of-not-having-it-3'
+                     break
+
+                   default:
+                     redirectUrl = req.path
+                     break
+                 }
+                 res.redirect(redirectUrl)
+               })
+
+               router.route('/alpha-apply-1c/1c-specialist-equipment-type-of-equipment-1')
+                 .post((req, res, next) => {
+                   let redirectUrl
+                   switch (req.body['add-another-1']) {
+                     case 'Yes':
+                       redirectUrl = '/alpha-apply-1c/1c-specialist-equipment-type-of-equipment-2'
+                       break
+                     case 'No':
+                       redirectUrl = '/alpha-apply-1c/1c-specialist-equipment-impact-of-not-having-it-1'
+                       break
+
+                     default:
+                       redirectUrl = req.path
+                       break
+                   }
+                   res.redirect(redirectUrl)
+                 })
+
+                 router.route('/alpha-apply-1c/1c-specialist-equipment-hmrc-table-2')
+                   .post((req, res, next) => {
+                     let redirectUrl
+                     switch (req.body['add-another-2']) {
+                       case 'Yes':
+                         redirectUrl = '/alpha-apply-1c/1c-specialist-equipment-type-of-equipment-3'
+                         break
+                       case 'No':
+                         redirectUrl = '/alpha-apply-1c/1c-specialist-equipment-impact-of-not-having-it-2'
+                         break
+
+                       default:
+                         redirectUrl = req.path
+                         break
+                     }
+                     res.redirect(redirectUrl)
+                   })
+
+
---
+++ node_modules/hmrc-frontend/hmrc/components/timeout-dialog/utils.js
@@ -0,0 +1,55 @@
+/* global ActiveXObject */
+
+import { nodeListForEach } from '../../common';
+
+const { warn } = console;
+
+const utils = {
+  generateDomElementFromString(str) {
+    const abc = document.createElement('div');
+    abc.innerHTML = str;
+    return abc.firstChild;
+  },
+
+  generateDomElementFromStringAndAppendText(str, text) {
+    const $tmp = utils.generateDomElementFromString(str);
+    $tmp.innerText = text;
+    return $tmp;
+  },
+
+  hasClass(selector, className) {
+    return document.querySelector(selector).classList.contains(className);
+  },
+
+  addClass(selector, className) {
+    const elements = document.querySelectorAll(selector);
+    nodeListForEach(elements, (i) => { i.classList.add(className); });
+  },
+
+  removeClass(selector, className) {
+    const elements = document.querySelectorAll(selector);
+    nodeListForEach(elements, (i) => { i.classList.remove(className); });
+  },
+
+  removeElement($elem) {
+    const parent = $elem.parentNode;
+    if (parent) {
+      parent.removeChild($elem);
+    } else {
+      warn("couldn't find parent for elem", $elem);
+    }
+  },
+
+  ajaxGet(url, success) {
+    const xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+    xhr.open('GET', url);
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState > 3 && xhr.status === 200) success(xhr.responseText);
+    };
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.send();
+    return xhr;
+  },
+};
+
+export default utils;
