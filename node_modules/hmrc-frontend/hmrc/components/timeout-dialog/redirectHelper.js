5c98a461712d608ee83d5cc7f64dd8eb4823767f
rachelderbyshire <84026497+rachelderbyshire@users.noreply.github.com>
2022-09-27 12:41:42 +0100

    kit update

--- node_modules/hmrc-frontend/hmrc/components/timeout-dialog/redirectHelper.js
+++
@@ -1,8 +0,0 @@
-function RedirectHelper() {}
-
-RedirectHelper.redirectToUrl = (url) => {
-  // This exists to make redirects more testable
-  window.location.href = url;
-};
-
-export default RedirectHelper;
--- node_modules/hmrc-frontend/hmrc/vendor/polyfills/Number/prototype/isNaN.js
+++
@@ -1,4 +0,0 @@
-// Polyfill from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isNaN
-Number.isNaN = Number.isNaN || function isNaN(input) {
-  return typeof input === 'number' && input !== input;
-}
